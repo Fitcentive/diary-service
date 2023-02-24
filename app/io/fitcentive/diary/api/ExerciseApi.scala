0d331e336df3a5c90a2fcb2f2feb9977d28c4571
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-02-23 22:41:42 -0500

    feat: Tweak API to return only english data

--- app/io/fitcentive/diary/api/ExerciseApi.scala
+++ app/io/fitcentive/diary/api/ExerciseApi.scala
@@ -13,11 +13,14 @@ import scala.util.Try
 @Singleton
 class ExerciseApi @Inject() (exerciseService: ExerciseService)(implicit ec: ExecutionContext) {
 
+  val ENGLISH = 2
+
   val defaultLimit = 50
   val defaultOffset = 0
 
   def getAllExerciseInfo: Future[Either[DomainError, Seq[ExerciseDefinition]]] =
     exerciseService.getCompleteExerciseDetailedInfo
+      .map(_.map(_.filter(_.language.id == ENGLISH)))
 
   implicit class OptionalStringToEmpty(s: Option[String]) {
     def optString: String =
