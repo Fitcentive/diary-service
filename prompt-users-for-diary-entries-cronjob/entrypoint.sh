1fd8034ca08f40f7b6a7ec3b00de2f5f8c131405
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-06-14 10:23:15 -0400

    Revert: "feat: Add cronjob to send users push notifications to enter diary entries once a day" - Reason: Need to only send out push notifs to those who have NOT added diary entries. Current approach might not be very scalable...

--- prompt-users-for-diary-entries-cronjob/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-gcloud auth activate-service-account --key-file=/opt/service-account/key.json
-gcloud pubsub topics publish --project=fitcentive-dev-02 prompt-users-for-diary-entries --message="{\"topic\":\"prompt-users-for-diary-entries\",\"payload\":{\"message\":\"Send push notification to all users to prompt them to enter diary entries now\"},\"id\":\"dbb8cbdf-62fd-40a4-b710-67c90141f3e3\"}"
-
-exec "$@"
