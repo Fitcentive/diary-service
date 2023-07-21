614034966d5c37fed62c17c87cef8fc314092a9d
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-07-20 22:26:38 -0400

    feat: Deploy service to fitcentive-dev-03

--- warm-wger-api-cache-cronjob/entrypoint.sh
+++ warm-wger-api-cache-cronjob/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 gcloud auth activate-service-account --key-file=/opt/service-account/key.json
-gcloud pubsub topics publish --project=fitcentive-dev-02 warm-wger-api-cache --message="{\"topic\":\"warm-wger-api-cache\",\"payload\":{\"message\":\"Warm wger API cache now\"},\"id\":\"b505fa53-1ca7-41f8-a0e9-a746882b9601\"}"
+gcloud pubsub topics publish --project=fitcentive-dev-03 warm-wger-api-cache --message="{\"topic\":\"warm-wger-api-cache\",\"payload\":{\"message\":\"Warm wger API cache now\"},\"id\":\"b505fa53-1ca7-41f8-a0e9-a746882b9601\"}"
 
 exec "$@"
