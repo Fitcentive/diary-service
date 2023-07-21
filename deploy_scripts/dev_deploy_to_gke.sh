cb61d09f74eb8ebefa440fc714b3e9734a03478f
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-07-20 23:49:54 -0400

    fix: Update deploy script

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -9,3 +9,7 @@ echo "y" | gcloud container images delete gcr.io/fitcentive-dev-03/diary:1.0 --f
 sbt docker:publish
 
 kubectl apply -f deployment/gke-dev-env/
+
+cd warm-wger-api-cache-cronjob && \
+  docker build -t gcr.io/fitcentive-dev-02/gcloud-diary-cron-pubsub-image:latest -t gcr.io/fitcentive-dev-02/gcloud-diary-cron-pubsub-image:1.0 . && \
+  docker push gcr.io/fitcentive-dev-03/gcloud-diary-cron-pubsub-image:1.0
